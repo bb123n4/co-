209835182e23c7e10d304521503d65f3d369f281
bb123n4 <11823009d@connect.polyu.hk>
2015-03-08 21:07:31 +0800

    add login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<div style="margin:0 auto">
+<form style="position:absolute; z-index:100; border-radius:5px;margin-top:25%;margin-left:45%;" class="bg-info"  >
+<div class="form-group" style="padding-left:10%;padding-right:10%;padding-top:10%">
+  <label for="Username">Username</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Username">
+  </div>
+  <div class="form-group" style="padding-left:10%;padding-right:10%">
+    <label for="Password">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div style="padding-bottom:10%;padding-left:10%;">
+  <button type="submit" class="btn btn-primary" >Log in</button>
+  </div>
+</form>
+</div>
