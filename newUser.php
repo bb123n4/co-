e5ef229ed3f3b3d425fba36163f60bde5ccfdf2b
cvcv101wh <cvcv101wh@163.com>
2015-03-23 23:38:44 +0800

    Update newUser.php

--- newUser.php
+++ newUser.php
@@ -1,3 +1,11 @@
 <?php
+//$newFileName=$_POST[];  //set the variable name
+$newFileName="testName.html";
+
+$htmlContent=""; 
+
+$newPage=fopen($newFileName,"w");
+fwrite($wAnswer,$htmlContent);
 
 ?>
+
